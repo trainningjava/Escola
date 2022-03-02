5a9926f5f99f71a8a9e7328c8343637d6e8f2bd5
trainningjava <48299029+trainningjava@users.noreply.github.com>
2022-03-02 18:56:27 -0300

    Create data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,14 @@
+insert into Turma (nome) values ('A1 - Manha');
+insert into Turma (nome) values ('A2 - Tarde');
+insert into Turma (nome) values ('A3 - Noite');
+insert into Turma (nome) values ('B1 - Manha');
+insert into Turma (nome) values ('B2 - Tarde');
+insert into Turma (nome) values ('B3 - Noite');
+insert into Turma (nome) values ('C1 - Manha');
+insert into Turma (nome) values ('C2 - Tarde');
+ 
+insert into Disciplina (nome) values ('Matematica');
+insert into Disciplina (nome) values ('Portugues');
+insert into Disciplina (nome) values ('Ingles');
+insert into Disciplina (nome) values ('Geografia');
+insert into Disciplina (nome) values ('Economia');
