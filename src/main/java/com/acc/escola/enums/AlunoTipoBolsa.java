f16a415e0bbb1bf5de6e38360ac87c84650ae87b
trainningjava <48299029+trainningjava@users.noreply.github.com>
2022-03-09 09:35:48 -0300

    Add files via upload

---
+++ src/main/java/com/acc/escola/enums/AlunoTipoBolsa.java
@@ -0,0 +1,19 @@
+package com.acc.escola.enums;
+
+public enum AlunoTipoBolsa {
+    B90 (90/100),
+    B50 (50/100),
+    B10 (10/100);
+
+    private int code;
+
+    AlunoTipoBolsa(int code) {
+        this.code = code;
+    }
+
+    public int getCode() {
+        return code;
+    }
+}
+
+
